56d261358e6cfa2943a1e4a2ff9fd6afd26d999c
川島 <>
2024-10-11 23:27:59 +0900

    better errosを導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,20 @@
-class Article < ApplicationRecord
-    validates :title, presence: true
-    validates :content, presence: true
+# frozen_string_literal: true
 
-    def display_created_at
-        I18n.l(self.created_at, format: :default)
-    end
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+
+  def display_created_at
+    I18n.l(created_at, format: :default)
+  end
 end
